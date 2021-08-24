43fcf628332ef2a6a4f5d52cbf8ccd9c9067f280
DTrombett <maxtromb.dt@gmail.com>
2021-08-24 13:44:49 +0200

    feat: add client events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { ClientOptions } from "discord.js";
+import type { ClientOptions, GuildMember, User } from "discord.js";
 import { Client, Constants, Options } from "discord.js";
 import { config } from "dotenv";
 import { assert } from "superstruct";
@@ -13,9 +13,12 @@ const invalidRequestWarningInterval = 1;
 const restGlobalRateLimit = 50;
 const restRequestTimeout = 10_000;
 const restTimeOffset = 1_000;
-const large_threshold = 1_000;
-const GuildMemberManager = 100;
-const UserManager = 1_000;
+
+const limitedManagerOptions = {
+	maxSize: 1,
+	sweepFilter: () => (userOrMember: GuildMember | User) =>
+		userOrMember.id !== userOrMember.client.user?.id,
+} as const;
 
 const options: ClientOptions = {
 	intents: (1 << IntentsFlags.GUILDS) | (1 << IntentsFlags.GUILD_MESSAGES),
@@ -32,21 +35,21 @@ const options: ClientOptions = {
 	shards: "auto",
 	userAgentSuffix: ["@DTrombett/github-bot@v1.0.0"],
 	ws: {
-		large_threshold,
+		large_threshold: 0,
 	},
 	makeCache: Options.cacheWithLimits({
 		...Options.defaultMakeCacheSettings,
 		BaseGuildEmojiManager: 0,
 		GuildBanManager: 0,
 		GuildInviteManager: 0,
+		GuildMemberManager: limitedManagerOptions,
 		GuildStickerManager: 0,
+		PresenceManager: 0,
 		ReactionManager: 0,
 		ReactionUserManager: 0,
 		StageInstanceManager: 0,
+		UserManager: limitedManagerOptions,
 		VoiceStateManager: 0,
-		PresenceManager: 0,
-		GuildMemberManager,
-		UserManager,
 	}),
 	rejectOnRateLimit({ global, limit, method, path, route, timeout }) {
 		console.warn(
@@ -61,14 +64,39 @@ const options: ClientOptions = {
 assert(token, sString);
 assert(options, sClientOptions);
 
-const client = new Client(options);
-new GitHubClient({ token, client }).login();
-
-client
-	.login()
-	.then(() => {
+const client = new Client(options)
+	.on(Constants.Events.CLIENT_READY, (readyClient) => {
 		console.log(
-			`Succesfully logged as ${client.user?.tag ?? "Unknown User"}. Waiting for guilds...`
+			`Succesfully logged as ${client.user?.tag ?? "Unknown User"} in ${
+				readyClient.guilds.cache.size
+			} guilds.`
 		);
 	})
-	.catch(console.error);
+	.on(Constants.Events.DEBUG, console.debug)
+	.on(Constants.Events.ERROR, console.error)
+	.on(Constants.Events.GUILD_CREATE, (guild) => {
+		console.log("Joined a new guild!");
+		console.log(guild);
+	})
+	.on(Constants.Events.GUILD_DELETE, (guild) => {
+		console.log("Left a guild!");
+		console.log(guild);
+	})
+	.on(Constants.Events.INVALIDATED, () => process.exit())
+	.on(Constants.Events.INVALID_REQUEST_WARNING, console.warn)
+	.on(Constants.Events.RATE_LIMIT, console.error)
+	.on(Constants.Events.WARN, console.warn)
+	.on(Constants.Events.SHARD_DISCONNECT, (event, shard) => {
+		console.error(`Shard ${shard} disconnected for an error!`);
+		console.error(event);
+	})
+	.on(Constants.Events.SHARD_RECONNECTING, (shard) => {
+		console.info(`Shard ${shard} is reconnecting...`);
+	})
+	.on(Constants.Events.SHARD_RESUME, (shard, events) => {
+		console.info(`Shard ${shard} resumed! Replayed ${events} events.`);
+	});
+
+new GitHubClient({ token, client }).login();
+
+client.login().catch(console.error);
